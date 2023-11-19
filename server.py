d4f770fd181b14dec25a2e4d3e4bdaa4a39033f3
Rinesa Zuzaku <119458053+RinesaZuzaku@users.noreply.github.com>
2023-11-19 19:42:30 +0100

    authenticate_client(username) and client_handler() functions

--- server.py
+++ server.py
@@ -1,8 +1,10 @@
 import socket
 import threading
 
-client_privileges = {"Rinesa" : "read"}
+client_privileges = {"Rinesa": "read"}
 
+def authenticate_client(username):
+    return username in client_privileges and client_privileges[username] == "write"
 def handle_client_request(data, address):
     try:
         username, privilege, operation, filename, content = data.decode().split("|")
@@ -10,7 +12,6 @@ def handle_client_request(data, address):
         response = "Invalid request format"
         server_socket.sendto(response.encode(), address)
         return
-
 
     if authenticate_client(username):
         try:
@@ -30,6 +31,16 @@ def handle_client_request(data, address):
         response = "Invalid username or privilege"
 
     server_socket.sendto(response.encode(), address)
+
+    # def authenticate_client(username):
+    # return username in client_privileges and client_privileges[username] == "write"
+
+def client_handler():
+    while True:
+        data, address = server_socket.recvfrom(1024)
+        client_thread = threading.Thread(target=handle_client_request, args=(data, address))
+        client_thread.start()
+
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
