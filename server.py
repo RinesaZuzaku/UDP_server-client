3133b57cca2f7ab143401582c89692a9d215721a
Rinesa Hoxha <rinesahoxha15@gmail.com>
2023-11-19 19:26:07 +0100

    handle client request function

--- server.py
+++ server.py
@@ -1,6 +1,35 @@
 import socket
 import threading
 
+client_privileges = {"Rinesa" : "read"}
+
+def handle_client_request(data, address):
+    try:
+        username, privilege, operation, filename, content = data.decode().split("|")
+    except ValueError:
+        response = "Invalid request format"
+        server_socket.sendto(response.encode(), address)
+        return
+
+
+    if authenticate_client(username):
+        try:
+            if privilege == "read":
+                with open(filename, "r") as file:
+                    response = file.read()
+            elif privilege == "write":
+                with open(filename, "a") as file:
+                    file.write("\n" + content)
+                response = "Write operation successful"
+            else:
+                response = "Invalid privilege"
+        except FileNotFoundError:
+            with open(filename, "w") as file:
+                response = "File not found. Created a new file."
+    else:
+        response = "Invalid username or privilege"
+
+    server_socket.sendto(response.encode(), address)
 
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
