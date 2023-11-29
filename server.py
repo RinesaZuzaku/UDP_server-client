52fd2cc0d2238aac203c64aef6021e82b443e06e
Rinesa Zuzaku <119458053+RinesaZuzaku@users.noreply.github.com>
2023-11-29 17:05:19 +0100

    authenticate_client(username) and client_handler() functions

--- server.py
+++ server.py
@@ -1,53 +1,37 @@
+# server.py
 import socket
 import threading
 
-client_privileges = {"Rinesa": "read"}
+# Dictionary to store client privileges based on their usernames
+client_privileges = {"user1": ["read", "write"], "user2": ["read"],"user3":["read"]}
 
-def authenticate_client(username):
-    return username in client_privileges and client_privileges[username] == "write"
 def handle_client_request(data, address):
     try:
-        username, privilege, operation, filename, content = data.decode().split("|")
+        username, operation, filename, content = data.decode().split("|")
     except ValueError:
         response = "Invalid request format"
         server_socket.sendto(response.encode(), address)
         return
 
-    if authenticate_client(username):
+    # Authenticate client and get the privilege
+    privilege = authenticate_client(username)
+
+    # Check if the client is authenticated
+    if privilege:
         try:
-            if privilege == "read":
+            if operation == "read" and "read" in privilege:
                 with open(filename, "r") as file:
                     response = file.read()
-            elif privilege == "write":
+            elif operation == "write" and "write" in privilege:
                 with open(filename, "a") as file:
                     file.write("\n" + content)
                 response = "Write operation successful"
             else:
-                response = "Invalid privilege"
+                response = "Invalid privilege for the specified operation"
         except FileNotFoundError:
             with open(filename, "w") as file:
                 response = "File not found. Created a new file."
     else:
-        response = "Invalid username or privilege"
+        response = "Invalid username"
 
     server_socket.sendto(response.encode(), address)
-
-
-
-def client_handler():
-    while True:
-        data, address = server_socket.recvfrom(1024)
-        client_thread = threading.Thread(target=handle_client_request, args=(data, address))
-        client_thread.start()
-
-
-
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_address = ("127.0.0.1", 12345)
-server_socket.bind(server_address)
-
-print("Server is listening on", server_address)
-while True:
-    user_input = input("Enter request (e.g., user1|write|write|example.txt|new data): ")
-    user_input = user_input.encode()
-    server_socket.sendto(user_input, server_address)
